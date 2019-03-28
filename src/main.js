67f7aed2f6c40266f691fd5a0913c7c9a7b21066
verdelejos <lucie.esparza@gmail.com>
2019-03-28 12:54:08 -0600

    Se agregó función de log out, y se mejoró el código de sign up y log in

--- src/main.js
+++ src/main.js
@@ -1,3 +1,14 @@
+// INITIALIZE FIREBASE
+var config = {
+    apiKey: "AIzaSyA5c3QxVhf1fbrXpiFFlQs2ZSgae2EPeIM",
+    authDomain: "zero-waste-d5038.firebaseapp.com",
+    databaseURL: "https://zero-waste-d5038.firebaseio.com",
+    projectId: "zero-waste-d5038",
+    storageBucket: "zero-waste-d5038.appspot.com",
+    messagingSenderId: "222059406012"
+};
+firebase.initializeApp(config);
+
 //SIGN UP FUNCTION
 function signUp  (e) {
     e.preventDefault(e);
@@ -20,7 +31,8 @@ function logIn (e) {
     e.preventDefault(e);
     let emailLogIn = document.getElementById("txtEmailLogIn").value;
     let passwordLogIn = document.getElementById("txtPasswordLogIn").value;
-
+    console.log("emailLogIn");
+    console.log("passwordLogIn");
     firebase.auth().signInWithEmailAndPassword(emailLogIn, passwordLogIn).catch(function(error) {
         // Handle Errors here.
         var errorCode = error.code;
@@ -30,6 +42,47 @@ function logIn (e) {
 }
 //LOG IN BUTTON
 document.getElementById("btnLogIn").addEventListener("click", logIn);
+
+
+//AUTHENTICATION OBSERVER FUNCTION 
+function authObserver () {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          // User is signed in.
+          document.getElementById("loggedIn").style.display="block";
+          document.getElementById("signUpForm").style.display="none";
+          document.getElementById("logInForm").style.display="none";
+         
+          // ...
+        } else {
+          // User is signed out.
+          document.getElementById("logInForm").style.display="block";
+          document.getElementById("signUpForm").style.display="block";
+          document.getElementById("loggedIn").style.display="none";
+          // ...
+        }
+      });
+}
+//RUNS AUTOMATICALLY WHEN FILE IS READ
+authObserver();
+
+
+//LOG OUT FUNCTION
+function logOut (e) {
+    e.preventDefault(e);
+
+    firebase.auth().signOut()
+    .then(function(){
+        console.log("Éxito");
+
+    })
+    .catch(function(error){
+        console.log(error);
+
+    })
+    
+}
+document.getElementById("btnLogOut").addEventListener('click', logOut);
 
 
 /* (function() {
